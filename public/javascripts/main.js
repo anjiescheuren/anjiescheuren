3eddf6ab5467efb3b884d0f145bf46fc9c1709be
Anjie Scheuren <emailanjie@gmail.com>
2016-02-26 15:43:11 -0600

    add ajax

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,13 +1,32 @@
 $(function() {
+  var apiRoot1 = 'http://api.giphy.com/v1/gifs/3ornjMThnISHhr4xKE?api_key=dc6zaTOxFJmzC';
+  var apiRoot2 = 'http://api.giphy.com/v1/gifs/3ornjRMoa2QP6vhT7a?api_key=dc6zaTOxFJmzC';
 
-  $('.hover').mouseover(function() {
-    $('.text').css("visibility","visible");
-  });
+  $.ajax({
+            url: apiRoot1,
+            method: "GET",
+            data: {},
+            dataType: "json",
+            jsonCallback: "info"
+        })
+  .done(function(data) {
+    var url = data.data.images.original.url;
+    console.log(url);
+    $('.goodwill').html('<img class="goodwill" src="/images/goodwill.gif">');
+  })
 
-  $('.hover').mouseout(function() {
-    $('.text').css("visibility","hidden");
-  });
-
+  $.ajax({
+            url: apiRoot2,
+            method: "GET",
+            data: {},
+            dataType: "json",
+            jsonCallback: "info"
+        })
+  .done(function(data) {
+    var url2 = data.data.images.original.url;
+    console.log(url2);
+    $('.gif').html('<img class="gif" src="/images/slideshow.gif">');
+  })
 });
 
 
